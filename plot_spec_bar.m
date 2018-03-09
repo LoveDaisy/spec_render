ba4f76e5803c2afe7cfc9740274646058d1d1606
zhangjiajie <zhangjiajie@kuaishou.com>
2018-03-09 12:02:40 +0800

    refactor. remove irrelevant files

--- plot_spec_bar.m
+++ plot_spec_bar.m
@@ -3,8 +3,6 @@ clear; clc; close all;
 %%
 lambda_store = (400:.1:800)';
 
-% sun_spectrum = ones(size(lambda_store));
-% sun_spectrum = sun_spectrum / sum(sun_spectrum);
 sun_spectrum = ones(size(lambda_store));
 sun_spectrum(550<lambda_store&lambda_store<555) = 0;
 
@@ -16,14 +14,6 @@ rgb = spec_to_rgb([lambda_store, sun_spectrum], 'Space', color_space, ...
     'MaxY', max_y);
 
 %%
-% img = ind2rgb(repmat(lambda_store-min(lambda_store), [1, 10])', rgb);
-
-% figure(1); clf;
-% set(gcf, 'Position', [100, 500, 800, 120]);
-% image(lambda_store, 1, img);
-% set(gca, 'FontSize', 18, 'YTick', [], 'XTick', 400:40:720);
-% xlabel('Wavelength (nm)', 'FontSize', 22);
-
 figure(2); clf;
 set(gcf, 'Position', [100, 500, 800, 420]);
 subplot('Position', [0.1, 0.3, 0.8, 0.6]);
